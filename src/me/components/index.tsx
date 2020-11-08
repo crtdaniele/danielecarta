2da4c12eea438e1031507cf4fd30a93552912d41
Daniele <crtdaniele@gmail.com>
2020-11-08 08:51:35 +0100

    fix: ogImage

--- src/me/components/index.tsx
+++ src/me/components/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Div, Img } from '../../_shared/theme/components';
 import InfoDanieleCarta from '../../assets/img/info-daniele-carta.png';
-import ogImage from '../../assets/img/ogImage.png';
-import Meta from '../../_shared/theme/components/meta';
 
 const Me = () => {
   return (
-    <>
-      <Meta ogImage={ogImage} />
-      <Div>
-        <Img fullWidth={true} src={InfoDanieleCarta} />
-      </Div>
-    </>
+    <Div>
+      <Img fullWidth={true} src={InfoDanieleCarta} />
+    </Div>
   );
 };
 
