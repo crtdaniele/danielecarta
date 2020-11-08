206a896086c0b480cbb0388cd94b19948a81505e
Daniele <crtdaniele@gmail.com>
2020-11-08 09:36:13 +0100

    fix: test ogImage

--- src/me/components/index.tsx
+++ src/me/components/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Div, Img } from '../../_shared/theme/components';
 import InfoDanieleCarta from '../../assets/img/info-daniele-carta.jpg';
+import ogImage from '../../assets/img/ogImage.png';
+import Meta from '../../_shared/theme/components/meta';
 
 const Me = () => {
   return (
-    <Div>
-      <Img fullWidth={true} src={InfoDanieleCarta} />
-    </Div>
+    <>
+      <Meta ogImage={ogImage} />
+      <Div>
+        <Img fullWidth={true} src={InfoDanieleCarta} />
+      </Div>
+    </>
   );
 };
 
